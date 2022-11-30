43d040c3d1a5b5e563dc63fcfca3be0257eee032
Federico Merafina <federico.merafina@losectrl.it>
2022-11-30 20:43:01 +0100

    feat: add javascript on project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function openAnswer(number){
+    let element = document.getElementById('answer-'+number);
+    let svg = document.getElementById('thin-x '+number);
+    let items = document.querySelectorAll('.faq-answer');
+    let itemsSvg = document.querySelectorAll('.svg-icon-thin-x');
+
+    this.checkClass(items,element);
+    this.checkClass(itemsSvg,svg);
+
+    return;
+}
+
+function checkClass(items,element){
+    items.forEach(e => {
+        if(e == element){
+            element.classList.toggle('open')
+        }else{
+            e.classList.remove('open');
+        }
+    });
+    return;
+}
